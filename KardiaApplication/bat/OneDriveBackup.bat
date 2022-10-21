14cc597fe4f88697a6dd0c98711bbdbfa2f6d5e2
robert.wadhams <robert.wadhams@gmail.com>
2022-10-21 15:25:51 +1100

    Improved OneDriveBackup script

--- KardiaApplication/bat/OneDriveBackup.bat
+++ KardiaApplication/bat/OneDriveBackup.bat
@@ -1,8 +1,8 @@
 @echo off
 REM OneDriveBackup for Kardia data files and reports
 
-REM data files
-copy *.xml %userprofile%\OneDrive\Documents\Kardia
+if not exist "%userprofile%\OneDrive\Documents\App_Data_and_Reporting_Backups\Kardia\" mkdir %userprofile%\OneDrive\Documents\App_Data_and_Reporting_Backups\Kardia
 
-REM report files
-copy out\*.txt %userprofile%\OneDrive\Documents\Kardia
+xcopy *.xml %userprofile%\OneDrive\Documents\App_Data_and_Reporting_Backups\Kardia /Y
+
+xcopy out\*.* %userprofile%\OneDrive\Documents\App_Data_and_Reporting_Backups\Kardia\out /I /Y
