9345cf7dd2140027b76592af6d610c66107cb897
robert.wadhams <robert.wadhams@gmail.com>
2022-11-11 17:48:44 +1100

    Add Log4j2 to application

--- KardiaApplication/src/main/dist/readme.txt
+++ KardiaApplication/src/main/dist/readme.txt
@@ -1,5 +1,5 @@
-TravelKilometres App
-====================
+Kardia App
+==========
 
 Setup
 -----
--- KardiaApplication/src/main/groovy/com/wadhams/kardia/app/KardiaApp.groovy
+++ KardiaApplication/src/main/groovy/com/wadhams/kardia/app/KardiaApp.groovy
@@ -12,6 +12,9 @@ import com.wadhams.kardia.dto.KardiaSymptom
 import com.wadhams.kardia.dto.ListRange
 import groovy.transform.ToString
 
+import groovy.util.logging.Log4j2
+
+@Log4j2 (value = 'logger')
 class KardiaApp {
 	DateTimeFormatter dtf = DateTimeFormatter.ofPattern('dd/MM/yyyy HH:mm')
 	DateTimeFormatter df = DateTimeFormatter.ofPattern('dd/MM/yyyy')
@@ -22,20 +25,17 @@ class KardiaApp {
 	List<KardiaSymptom> ksList
 
 	static main(args) {
-		println 'KardiaApp started...'
-		println ''
+		logger.info 'KardiaApp started...'
 
 		KardiaApp app = new KardiaApp()
 		app.execute()
 		
-		println ''
-		println 'KardiaApp ended.'
+		logger.info 'KardiaApp ended.'
 	}
 	
 	def execute() {
 		String kFilename = 'Kardia.xml'
-		println "Processing: $kFilename"
-		println ''
+		logger.info "Processing: $kFilename"
 		
 		File kFile
 		URL resource = getClass().getClassLoader().getResource(kFilename)
@@ -64,19 +64,27 @@ class KardiaApp {
 //		println ksList
 //		println ''
 		
-		File f = new File("out/kardia-report.txt")
-		f.withPrintWriter {pw ->
+		File f1 = new File("out/kardia-report.txt")
+		f1.withPrintWriter {pw ->
 			reportReadings(pw)
 			reportTotals(pw)
 			reportMedications(pw)
 			reportSymptoms(pw)
 		}
 
+		File f2 = new File("out/kardia-report.html")
+		f2.withPrintWriter {pw ->
+			pw.println '<html>'
+			reportReadingsHTML(pw)
+			reportTotalsHTML(pw)
+			reportMedicationsHTML(pw)
+			reportSymptomsHTML(pw)
+			pw.println '</html>'
+		}
+
 	}
 	
 	def reportReadings(PrintWriter pw) {
-		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern('dd/MM/yyyy')
-		
 		listRangeList.each {rl ->
 			List<KardiaReading> subList = krList.subList(rl.startIndex, rl.endIndex+1)
 			//println subList
@@ -84,14 +92,40 @@ class KardiaApp {
 			def avg = subList.average {it.rate}
 			def kMax = subList.max {it.rate}
 			String resultName = (subList[0].result == 'NSR') ? 'Normal Sinus Rhythm............: ' : 'Possible Atrial Fibrillation...: '
-			String startDate = subList[0].dateTime.format(dtf1)
-			String endDate = subList[-1].dateTime.format(dtf1)
+			String startDate = subList[0].dateTime.format(dtf)
+			String endDate = subList[-1].dateTime.format(dtf)
 			long days = ChronoUnit.DAYS.between(subList[0].dateTime.toLocalDate(), subList[-1].dateTime.toLocalDate()) + 1
 			String average = (avg as int).toString().padRight(3, ' ')
 			
 			pw.println "$resultName${startDate} - ${endDate} (${days} days)\tAverage: ${average}\tMin/Max: ${kMin.rate}/${kMax.rate}"
 		}
 		pw.println ''
+	}
+	
+	def reportReadingsHTML(PrintWriter pw) {
+		pw.println '<b>Readings:</b>'
+		pw.println '<table border="1">'
+		listRangeList.each {rl ->
+			List<KardiaReading> subList = krList.subList(rl.startIndex, rl.endIndex+1)
+			//println subList
+			def kMin = subList.min {it.rate}
+			def avg = subList.average {it.rate}
+			def kMax = subList.max {it.rate}
+			String resultName = (subList[0].result == 'NSR') ? 'Normal Sinus Rhythm' : 'Possible Atrial Fibrillation'
+			String startDate = subList[0].dateTime.format(dtf)
+			String endDate = subList[-1].dateTime.format(dtf)
+			long days = ChronoUnit.DAYS.between(subList[0].dateTime.toLocalDate(), subList[-1].dateTime.toLocalDate()) + 1
+			String average = (avg as int).toString().padRight(3, ' ')
+			
+			pw.println '<tr>'
+			pw.println "<td>$resultName</td>"
+			pw.println "<td>${startDate} - ${endDate}</td>"
+			pw.println "<td>$days days</td>"
+			pw.println "<td>Average: ${average}</td>"
+			pw.println "<td>Min/Max: ${kMin.rate}/${kMax.rate}</td>"
+			pw.println '</tr>'
+		}
+		pw.println '</table>'
 	}
 	
 	def reportTotals(PrintWriter pw) {
@@ -108,6 +142,19 @@ class KardiaApp {
 		pw.println ''
 	}
 	
+	def reportTotalsHTML(PrintWriter pw) {
+		long totalDays = ChronoUnit.DAYS.between(krList[0].dateTime.toLocalDate(), krList[-1].dateTime.toLocalDate()) + 1
+		
+		long pafDays = 0
+		listRangeList.each {rl ->
+			List<KardiaReading> subList = krList.subList(rl.startIndex, rl.endIndex+1)
+			if (subList[0].result == 'PAF') {
+				pafDays += ChronoUnit.DAYS.between(subList[0].dateTime.toLocalDate(), subList[-1].dateTime.toLocalDate()) + 1
+			}
+		}
+		pw.println "<p>Total reporting period: $totalDays days. Possible Atrial Fibrillation: $pafDays days.</p>"
+	}
+	
 	def reportMedications(PrintWriter pw) {
 		int maxMedicationLength = 0
 		kmList.each {m ->
@@ -118,9 +165,22 @@ class KardiaApp {
 		
 		kmList.each {m ->
 			long totalDays = ChronoUnit.DAYS.between(m.start, m.end) + 1
-			pw.println "Medication: ${m.name.padRight(maxMedicationLength, ' ')} Start: ${m.start} End: ${m.end} ($totalDays days)"
+			pw.println "Medication: ${m.name.padRight(maxMedicationLength, ' ')} Start: ${m.start.format(df)} End: ${m.end.format(df)} ($totalDays days)"
 		}
 		pw.println ''
+	}
+	
+	def reportMedicationsHTML(PrintWriter pw) {
+		pw.println '<b>Medication:</b>'
+		pw.println '<table border="1">'
+		kmList.each {m ->
+			pw.println '<tr>'
+			long totalDays = ChronoUnit.DAYS.between(m.start, m.end) + 1
+			pw.println "<td>${m.name}</td>"
+			pw.println "<td>Start: ${m.start.format(df)} End: ${m.end.format(df)} ($totalDays days)</td>"
+			pw.println '</tr>'
+		}
+		pw.println '</table>'
 	}
 	
 	def reportSymptoms(PrintWriter pw) {
@@ -133,9 +193,22 @@ class KardiaApp {
 		
 		ksList.each {s ->
 			long totalDays = ChronoUnit.DAYS.between(s.start, s.end) + 1
-			pw.println "Symptom: ${s.name.padRight(maxSymptomLength, ' ')} Result: ${s.result} Start: ${s.start} End: ${s.end} ($totalDays days)"
+			pw.println "Symptom: ${s.name.padRight(maxSymptomLength, ' ')} Result: ${s.result} Start: ${s.start.format(df)} End: ${s.end.format(df)} ($totalDays days)"
 		}
 		pw.println ''
+	}
+	
+	def reportSymptomsHTML(PrintWriter pw) {
+		pw.println '<b>Symptoms:</b>'
+		pw.println '<table border="1">'
+		ksList.each {s ->
+			pw.println '<tr>'
+			long totalDays = ChronoUnit.DAYS.between(s.start, s.end) + 1
+			pw.println "<td>${s.name}</td>"
+			pw.println "<td>Result: ${s.result} Start: ${s.start.format(df)} End: ${s.end.format(df)} ($totalDays days)</td>"
+			pw.println '</tr>'
+		}
+		pw.println '</table>'
 	}
 	
 	def buildListRange() {
